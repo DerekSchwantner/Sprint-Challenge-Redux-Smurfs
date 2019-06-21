3f2e741c3e27e57b095bed83a89d31e31812a8a8
Derek S <dschwan21@me.com>
2019-06-21 09:52:55 -0700

    functional and working, just trying to fix refresh, smurfs/src/components/App.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,14 @@
+import axios from "axios";
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+export const FETCH_SMURFS_START = "FETCH_SMURFS_START";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_FAILURE = "FETCH_SMURFS_FAILURE";
 
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,27 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURFS_FAILURE, payload: err });
+    });
+};
+
+export const addSmurf = newSmurf => dispatch => {
+  axios
+    .post("http://localhost:3333/smurfs", newSmurf)
+    .then(res => {
+      // console.log("new smurfs array", res.data);
+      dispatch({ type: ADD_SMURF, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: ADD_SMURF_FAILURE, payload: err.response });
+    });
+};
